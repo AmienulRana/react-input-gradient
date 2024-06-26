73735655b6e0a77f0a5057ddcd8d2d5952bec25b
AmienulRana <amienulrana@gmail.com>
2024-06-26 11:47:43 +0700

    add state color and handle on change

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import { useState } from "react";
 import ColorPicker from "react-best-gradient-color-picker";
 
-export default ColorPicker;
+export default function InputColorPickerGradient() {
+  const [color, setColor] = useState("");
+  return <ColorPicker value={color} setColor={(value) => setColor(value)} />;
+}
