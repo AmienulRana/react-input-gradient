cfee2c6455b425b531c8576f9e70e5fb3307a37b
AmienulRana <amienulrana@gmail.com>
2024-06-27 09:03:48 +0700

    post message to other parent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ColorPicker from "react-best-gradient-color-picker";
 
 export default function InputColorPickerGradient() {
-  const [color, setColor] = useState("");
+  const [color, setColor] = useState("rgb(0,0,0)");
+  useEffect(() => {
+    // Send the color value to the parent window whenever it changes
+    if (window.parent) {
+      // console.log(window.parent);
+      window.parent.postMessage({ color: color }, "*");
+    }
+  }, [color]);
   return <ColorPicker value={color} onChange={(value) => setColor(value)} />;
 }
