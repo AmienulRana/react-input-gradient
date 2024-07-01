e087934645d65a55af28a179c96924d56829b109
AmienulRana <amienulrana@gmail.com>
2024-07-01 22:50:20 +0700

    update gradient

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import ColorPicker from "react-best-gradient-color-picker";
 
 export default function InputColorPickerGradient() {
   const [color, setColor] = useState("rgb(0,0,0)");
+
+  // useEffect(() => {
+  //   function handleMessage(event) {
+  //     if (event.origin === "http://localhost:3001") {
+  //       if (event.data.defaultColor) {
+  //         setColor(event.data.defaultColor);
+  //       }
+  //     }
+  //   }
+
+  //   window.addEventListener("message", handleMessage, false);
+
+  //   return () => {
+  //     window.removeEventListener("message", handleMessage);
+  //   };
+  // }, []);
   useEffect(() => {
     // Send the color value to the parent window whenever it changes
     if (window.parent) {
